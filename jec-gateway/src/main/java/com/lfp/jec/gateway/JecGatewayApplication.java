3b7a691cffe85233725ab7af35692ffd03672835
zhutao <zt@buaa.edu.cn>
2018-02-07 23:06:34 +0800

    修改配置服务的配置

--- jec-gateway/src/main/java/com/lfp/jec/gateway/JecGatewayApplication.java
+++ jec-gateway/src/main/java/com/lfp/jec/gateway/JecGatewayApplication.java
@@ -2,12 +2,14 @@ package com.lfp.jec.gateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 
 @SpringBootApplication
 @EnableEurekaClient		//声明为一个服务提供者
 @EnableZuulProxy		//声明为一个路由网关
+@EnableDiscoveryClient
 public class JecGatewayApplication {
 
 	public static void main(String[] args) {
